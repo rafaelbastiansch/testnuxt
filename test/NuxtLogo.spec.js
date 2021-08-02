35c94e257ad69f074c38c0020654cb76cee0ba36
Rafael <rafaelbastiansch@gmail.com>
2021-08-01 22:20:01 -0300

    teste

--- test/NuxtLogo.spec.js
+++ test/NuxtLogo.spec.js
@@ -1,7 +1,7 @@
 import { mount } from '@vue/test-utils'
 import NuxtLogo from '@/components/NuxtLogo.vue'
 
-describe('NuxtLogo teste', () => {
+describe('NuxtLogo tes', () => {
   test('is a Vue instance', () => {
     const wrapper = mount(NuxtLogo)
     expect(wrapper.vm).toBeTruthy()
